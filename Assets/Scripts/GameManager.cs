4bdac01c88ce1296d15af643f39a35e0a52d4169
iida <iida.lintuaho@gmail.com>
2024-03-28 21:35:23 +0200

    started working on menus

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,23 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //TODO make this manager static. see picross project
+    [SerializeField]
+    private Canvas m_menuCanvas;
+    [SerializeField]
+    private GameObject m_startView;
+    [SerializeField]
+    private GameObject m_endView;
+    public void StartGame()
+    {
+        m_menuCanvas.enabled = false;
+    }
+    public void EndGame()
+    {
+        m_menuCanvas.enabled = true;
+        m_startView.SetActive(false);
+        m_endView.SetActive(true);
+    }
 }
